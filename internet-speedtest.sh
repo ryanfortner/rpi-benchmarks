cabc1049ddcefb4cb6756afee90e1b7682531db0
chunkymilk0309 <71036629+chunky-milk@users.noreply.github.com>
2021-01-03 11:37:49 -0500

    Create internet-speedtest.sh

---
+++ internet-speedtest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! `which speedtest-cli` ]; then
+  echo "Installing speedtest-cli package from repositories"
+  sleep 2
+  sudo apt-get update
+  sudo apt-get install -y speedtest-cli
+fi
+
+clear
+/usr/bin/speedtest-cli --simple
+
+echo ""
+echo "Close this window to exit."
+echo ""
+sleep 1h
