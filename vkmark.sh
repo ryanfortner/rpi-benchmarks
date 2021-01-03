85f277b85f6fdfe326e906d57399a8409fd9ab55
chunkymilk0309 <71036629+chunky-milk@users.noreply.github.com>
2021-01-03 11:55:53 -0500

    Update vkmark.sh

--- vkmark.sh
+++ vkmark.sh
@@ -4,10 +4,10 @@ if [ ! `which vkmark` ]; then
   echo "Downloading and installing vkmark.deb."
   sleep 2
   sudo apt-get update
-  wget -O ~/drivers.deb https://github.com/chunky-milk/rpi-benchmarks/raw/main/mesa-vulkan-drivers-rpi4.deb | error "Failed to download drivers!"
-  sudo apt install --fix-missing -y ~/drivers.deb | error "Failed to install mesa & vulkan drivers!"
-  wget -O ~/vkmark.deb https://github.com/chunky-milk/rpi-benchmarks/raw/main/vkmark.deb | error "Failed to download vkmark!"
+  wget -O ~/drivers.deb https://github.com/chunky-milk/rpi-benchmarks/raw/main/mesa-vulkan-drivers-rpi4.deb
+  wget -O ~/vkmark.deb https://github.com/chunky-milk/rpi-benchmarks/raw/main/vkmark.deb
   sudo apt install --fix-missing -y ~/vkmark.deb
+  clear
 fi
 
 clear
