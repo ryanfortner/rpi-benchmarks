115b8c2d0cb520b7f5c3d51a895fa8d7d760ea46
chunkymilk0309 <71036629+chunky-milk@users.noreply.github.com>
2021-01-04 13:27:57 -0500

    Update install.sh

--- glmark2/install.sh
+++ glmark2/install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-wget 
+wget https://github.com/chunky-milk/rpi-benchmarks/raw/main/glmark2/glmark2.deb
+wget https://github.com/chunky-milk/rpi-benchmarks/raw/main/mesa-vulkan-drivers-rpi4.deb
+sudo apt install --fix-missing -y ~/mesa-vulkan-drivers-rpi4.deb
+sudo apt install --fix-missing -y ~/glmark2.deb
+
+echo "Done"
