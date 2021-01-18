ad81c49584187a42b7b972aec2183671f217df28
chunkymilk0309 <71036629+chunky-milk@users.noreply.github.com>
2021-01-18 11:21:26 -0500

    Update webgl.sh

--- webgl.sh
+++ webgl.sh
@@ -13,4 +13,4 @@ if [ ! `which chromium-browser` ]; then
 fi
 
 # Launch a web app which contains WebGL samples
-chromium-browser %U --user-agent="Mozilla/5.0 (X11; CrOS armv7l 11895.95.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.125 Safari/537.36" --app=https://webglsamples.org || error "Failed to launch application!"
+chromium-browser %U --user-agent="Mozilla/5.0 (X11; CrOS armv7l 11895.95.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.125 Safari/537.36" --app=https://chunky-milk.github.io/WebGLsamples/ || error "Failed to launch application!"
